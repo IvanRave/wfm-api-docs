109c45c155f6381eeeb6ceb2b54c236e1329296d
Ivan <ivanich86@gmail.com>
2014-07-17 20:40:54 +0400

    fix(auth): fix auth url

--- public/js/cnst-helper.js
+++ public/js/cnst-helper.js
@@ -3,7 +3,7 @@
 
 	// REQ
 	app.cnst.API_URI = 'https://wfm-report.herokuapp.com';
-  ////'http://localhost:3000';
+  //// 'http://localhost:3000';
   
   app.cnst.BASE_CONTAINER = 'swagger-ui-container';
 
@@ -12,8 +12,9 @@
 	app.cnst.auth = {};
 
 	app.cnst.auth.ID_OF_AUTH_CLIENT = 'api-docs';
-	app.cnst.auth.REDIRECT_URI = '//localhost:39393/handle-auth-code.html';
+	app.cnst.auth.REDIRECT_URI = '//ivanrave.github.io/wfm-api-docs' + '/handle-auth-code.html';
+  //// '//localhost:39393';
 
-	// var authUrl = isProd ? '//petrohelp-auth.herokuapp.com' : 'http://localhost:1337';
-	app.cnst.auth.AUTH_URI = 'http://localhost:1337/dialog/authorize';
+	app.cnst.auth.AUTH_URI = '//petrohelp-auth.herokuapp.com' + '/dialog/authorize'
+  //// 'http://localhost:1337';
 })(window.wfm);
