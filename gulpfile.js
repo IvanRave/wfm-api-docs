0fa3adc989c8e86c3c107fa1e13a82c4e470862c
Ivan <ivanich86@gmail.com>
2014-08-05 22:46:03 +0400

    chore(app): add build tasks
    clean dst folder; copy all files; set release config; gulp tasks
    
    time: 0h:40

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jshintReporter = require('jshint-stylish');
-var deploy = require('gulp-gh-pages');
+var ghPages = require('gulp-gh-pages');
+var del = require('del');
+var rename = require('gulp-rename');
 
 var filePath = {
 	jshintBase : {
@@ -12,6 +14,11 @@ var filePath = {
 	},
 	jshintTest : {
 		src : 'test/**/*.js'
+	},
+	app : {
+		src : './public/',
+		dst : './dst/',
+		dev : './dev/'
 	}
 };
 
@@ -31,7 +38,27 @@ gulp.task('test', function () {
 	.pipe(jshint.reporter(jshintReporter));
 });
 
+/** Clean destination folder */
+gulp.task('clean', function (cb) {
+	del([filePath.app.dst], cb);
+});
+
+// Copy all static files
+gulp.task('copySource', ['clean'], function () {
+	return gulp.src(filePath.app.src + '**/*')
+	.pipe(gulp.dest(filePath.app.dst));
+});
+
+gulp.task('renameConfig', ['clean', 'copySource'], function () {
+	return gulp.src(filePath.app.dst + 'js/cnst-helper-release.js')
+	.pipe(rename('js/cnst-helper.js'))
+	.pipe(gulp.dest(filePath.app.dst));
+});
+
+// Auto clean and copy all files
+gulp.task('build', ['renameConfig']);
+
 // https://github.com/rowoot/gulp-gh-pages
-gulp.task('deploy', function () {
-	gulp.src("./public/**/*").pipe(deploy());
+gulp.task('gh-pages', function () {
+	gulp.src(filePath.app.dst + "**/*").pipe(ghPages());
 });
